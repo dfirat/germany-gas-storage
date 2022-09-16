97eca39bebe95c04c863d0e2e2c484989fc25d27
dfirat <d.firat@mail.de>
2022-09-16 09:32:49 +0200

    Update

--- germany-gas-storage.R
+++ germany-gas-storage.R
@@ -8,35 +8,29 @@ library("hms")
 library("jsonlite")
 
 # Import data sets [(Source)](https://agsi.gie.eu/)
-de <- fromJSON("StorageData_GIE_2012-09-10_2022-09-10.json", flatten = TRUE)
-
+de <- fromJSON("StorageData_GIE_2016-04-07_2022-09-16.json", flatten = TRUE)
 
 # Add new year and month column
 de$year <- format(as.Date(de$gasDayStart), "%Y")
 de$month <- format(as.Date(de$gasDayStart), "%m-%d")
 
-
 # Exclude 2022 for calculations
 de_edit <- de[de$year != 2022, ]
-
 
 # Calcuate daily average
 de_average <- de_edit %>% 
   group_by(month) %>% 
   summarise(Mean = mean(full))
 
-
 # Calcuate daily maximum
 de_max <- de_edit %>% 
   group_by(month) %>% 
   summarise(Max = max(full))
 
-
 # Calcuate daily minimum
 de_min <- de_edit %>% 
   group_by(month) %>% 
   summarise(Min = min(full))
-
 
 # Split data set into each year
 de2022 <- de[de$year == 2022, ]
@@ -45,11 +39,6 @@ de2020 <- de[de$year == 2020, ]
 de2019 <- de[de$year == 2019, ]
 de2018 <- de[de$year == 2018, ]
 de2017 <- de[de$year == 2017, ]
-de2016 <- de[de$year == 2016, ]
-de2015 <- de[de$year == 2015, ]
-de2014 <- de[de$year == 2014, ]
-de2013 <- de[de$year == 2013, ]
-
 
 # Save new data set
 write.csv(de, "de-gas-storage.csv")
@@ -62,7 +51,3 @@ write.csv(de2020, "de-gas-storage2020.csv")
 write.csv(de2019, "de-gas-storage2019.csv")
 write.csv(de2018, "de-gas-storage2018.csv")
 write.csv(de2017, "de-gas-storage2017.csv")
-write.csv(de2016, "de-gas-storage2016.csv")
-write.csv(de2015, "de-gas-storage2015.csv")
-write.csv(de2014, "de-gas-storage2014.csv")
-write.csv(de2013, "de-gas-storage2013.csv")
